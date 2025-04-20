27b3b1c55e537a43e43f519e12add76fd8e41e8d
권장순 <jsoonworld@gmail.com>
2025-04-20 14:22:56 +0900

    feat(project) setup project

---
+++ src/main/kotlin/com/turning/server/kotlin/ServerKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.turning.server.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/turning/server/kotlin/ServerKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.turning.server.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
