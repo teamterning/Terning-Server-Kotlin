98b7aa32ee0897b93dfc94c56297472b8e4cf9eb
권장순 <jsoonworld@gmail.com>
2025-04-21 21:09:15 +0900

    feat(ExceptionHandler) : 전역 예외 처리 클래스 구현

---
+++ src/main/kotlin/com/terning/server/kotlin/ui/api/ExceptionHandler.kt
@@ -0,0 +1,70 @@
+package com.terning.server.kotlin.ui.api
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
+import jakarta.persistence.EntityNotFoundException
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@RestControllerAdvice
+class ExceptionHandler : ResponseEntityExceptionHandler() {
+    override fun handleHttpMessageNotReadable(
+        ex: HttpMessageNotReadableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        logger.error("message", ex)
+        val message = when (val exception = ex.cause) {
+            is MismatchedInputException -> "${exception.path.lastOrNull()?.fieldName.orEmpty()}: 널이어서는 안됩니다"
+            is InvalidFormatException -> "${exception.path.lastOrNull()?.fieldName.orEmpty()}: 올바른 형식이어야 합니다"
+            else -> exception?.message.orEmpty()
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ApiResponse.error(message))
+    }
+
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        logger.error("message", ex)
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ApiResponse.error(ex.messages()))
+    }
+
+    @ExceptionHandler(IllegalArgumentException::class, IllegalStateException::class)
+    fun handleBadRequestException(exception: RuntimeException): ResponseEntity<ApiResponse<Unit>> {
+        logger.error("message", exception)
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ApiResponse.error(exception.message))
+    }
+
+    @ExceptionHandler(EntityNotFoundException::class)
+    fun handleNotFoundException(exception: EntityNotFoundException): ResponseEntity<ApiResponse<Unit>> {
+        logger.error("message", exception)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(ApiResponse.error(exception.message))
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleGlobalException(exception: Exception): ResponseEntity<ApiResponse<Unit>> {
+        logger.error("message", exception)
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ApiResponse.error(exception.message))
+    }
+
+    private fun MethodArgumentNotValidException.messages(): String {
+        return bindingResult.fieldErrors.joinToString(", ") { "${it.field}: ${it.defaultMessage.orEmpty()}" }
+    }
+}
