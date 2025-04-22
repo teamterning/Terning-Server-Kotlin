aa8849791efb7bc404564a046eec14cf986ea8a9
권장순 <jsoonworld@gmail.com>
2025-04-23 08:13:37 +0900

    refactor(ExceptionHandler) : MethodArgumentNotValidException 메시지 표현식을 간결한 방식으로 리팩터링

--- src/main/kotlin/com/terning/server/kotlin/ui/api/ExceptionHandler.kt
+++ src/main/kotlin/com/terning/server/kotlin/ui/api/ExceptionHandler.kt
@@ -71,9 +71,8 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
             .body(ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."))
     }
 
-    private fun MethodArgumentNotValidException.messages(): String {
-        return bindingResult.fieldErrors.joinToString(", ") {
+    private fun MethodArgumentNotValidException.messages(): String =
+        bindingResult.fieldErrors.joinToString(", ") {
             "${it.field}: ${it.defaultMessage.orEmpty()}"
         }
-    }
 }
