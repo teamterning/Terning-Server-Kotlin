65ebc04600fadf768b3ec77ea0796e1a7af773b3
LEE YOU BIN <youbeene3409@naver.com>
2025-05-14 03:40:14 +0900

    feat(BaseRootEntity) : BaseJpaEntity 구현

--- src/main/kotlin/com/terning/server/kotlin/domain/user/BaseRootEntity.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/user/BaseRootEntity.kt
@@ -2,14 +2,30 @@ package com.terning.server.kotlin.domain.user
 
 import jakarta.persistence.*
 import java.time.LocalDateTime
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseJpaEntity {
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    lateinit var createdAt: LocalDateTime
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    lateinit var updatedAt: LocalDateTime
+}
+
 
 @MappedSuperclass
 abstract class BaseRootEntity<T>(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0L,
-    @Column(nullable = false)
-    val createdAt: LocalDateTime = LocalDateTime.now(),
-    @Column(nullable = false)
-    var updatedAt: LocalDateTime = LocalDateTime.now(),
-)
+    val id: Long = 0L
+) : BaseJpaEntity()
