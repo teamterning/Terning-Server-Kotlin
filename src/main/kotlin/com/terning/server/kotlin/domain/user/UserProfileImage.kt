fb246dd4d334fb885992ea481f0c1299412c4677
LEE YOU BIN <youbeene3409@naver.com>
2025-05-14 05:01:50 +0900

    feat(UserProfileImage) : UserProfileImage VO 구현

---
+++ src/main/kotlin/com/terning/server/kotlin/domain/user/UserProfileImage.kt
@@ -0,0 +1,13 @@
+package com.terning.server.kotlin.domain.user
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+
+@Embeddable
+data class UserProfileImage(
+    @Enumerated(EnumType.STRING)
+    @Column(length = 12)
+    val profileImage: ProfileImage
+)
