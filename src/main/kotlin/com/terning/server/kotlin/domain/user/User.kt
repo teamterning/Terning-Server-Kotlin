f73346991ced237f3f519e4454f8b7602beb2e4a
LEE YOU BIN <youbeene3409@naver.com>
2025-05-13 22:18:32 +0900

    feat(User) : User 엔티티 클래스 구현

---
+++ src/main/kotlin/com/terning/server/kotlin/domain/user/User.kt
@@ -0,0 +1,20 @@
+package com.terning.server.kotlin.domain.user
+
+import jakarta.persistence.*
+
+@Entity
+class User(
+    @Embedded
+    var userInformation: UserInformation,
+    @Enumerated(EnumType.STRING)
+    @Column(length = 12)
+    var userState: UserState,
+    id: Long = 0L,
+) : BaseRootEntity<User>(id) {
+    fun updateProfile(
+        name: String,
+        profileImage: ProfileImage,
+    ) {
+        this.userInformation = UserInformation(name = name, profileImage = profileImage)
+    }
+}
