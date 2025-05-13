85fb0b57b99dce9c05b7d8e2df5043b9e761c95c
LEE YOU BIN <youbeene3409@naver.com>
2025-05-13 22:56:36 +0900

    feat(User) : getName 메소드 추가

--- src/main/kotlin/com/terning/server/kotlin/domain/user/User.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/user/User.kt
@@ -11,6 +11,7 @@ class User(
     var userState: UserState,
     id: Long = 0L,
 ) : BaseRootEntity<User>(id) {
+    fun getName(): String = userInformation.name
     fun updateProfile(
         name: String,
         profileImage: ProfileImage,
