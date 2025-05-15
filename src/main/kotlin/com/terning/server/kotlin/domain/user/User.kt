82945f5b4ede2992caa6a27c6814138cf8856c50
권장순 <jsoonworld@gmail.com>
2025-05-15 17:34:11 +0900

    refactor(User) : 생성자와 메서드 파라미터 포맷 정리 및 import 명시화
    - @Id, @GeneratedValue annotation을 한 줄로 정리
    - updateProfile 파라미터를 개행하여 가독성 개선
    - import 정리로 명확성과 일관성 향상

--- src/main/kotlin/com/terning/server/kotlin/domain/user/User.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/user/User.kt
@@ -1,32 +1,40 @@
 package com.terning.server.kotlin.domain.user
 
 import com.terning.server.kotlin.utils.BaseRootEntity
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Embedded
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "users")
 class User(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L,
-
     @Embedded
     private var name: UserName,
-
     @Enumerated(EnumType.STRING)
     @Column(name = "profile_image", length = 12, nullable = false)
     private var profileImage: ProfileImage,
-
     @Enumerated(EnumType.STRING)
     @Column(name = "user_state", length = 12, nullable = false)
     private var userState: UserState,
 ) : BaseRootEntity() {
-
     fun name(): String = name.value
+
     fun profileImage(): ProfileImage = profileImage
+
     fun userState(): UserState = userState
 
-    fun updateProfile(newName: UserName, newProfileImage: ProfileImage) {
+    fun updateProfile(
+        newName: UserName,
+        newProfileImage: ProfileImage,
+    ) {
         this.name = newName
         this.profileImage = newProfileImage
     }
