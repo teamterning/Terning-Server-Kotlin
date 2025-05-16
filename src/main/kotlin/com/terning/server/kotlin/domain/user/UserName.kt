b953f986cd6fab4b46b8ef9820e16d4f982ed43c
LEE YOU BIN <youbeene3409@naver.com>
2025-05-16 11:38:34 +0900

    feat(UserName) : 정적팩토리함수 제거

--- src/main/kotlin/com/terning/server/kotlin/domain/user/UserName.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/user/UserName.kt
@@ -25,7 +25,5 @@ class UserName private constructor(
 
         private const val ERROR_EMPTY = "이름은 공백일 수 없습니다."
         private const val ERROR_LENGTH = "이름은 ${MIN_LENGTH}~${MAX_LENGTH}자여야 합니다."
-
-        fun from(value: String): UserName = UserName(value)
     }
 }
