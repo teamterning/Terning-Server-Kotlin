bd0ba8a57ed569499d9d626d627a16415fc65187
LEE YOU BIN <youbeene3409@naver.com>
2025-05-18 02:49:10 +0900

    feat(AuthErrorCode) : getPrefixMessage 함수 네이밍 변경

--- src/main/kotlin/com/terning/server/kotlin/domain/auth/AuthErrorCode.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/auth/AuthErrorCode.kt
@@ -9,7 +9,7 @@ enum class AuthErrorCode(
     INVALID_TOKEN(status = HttpStatus.UNAUTHORIZED, message = "유효하지 않은 토큰입니다."),
     ;
 
-    fun getMessage(): String = "$PREFIX $message"
+    fun getPrefixMessage(): String = "$PREFIX $message"
 
     companion object {
         private const val PREFIX = "[AUTH ERROR]"
