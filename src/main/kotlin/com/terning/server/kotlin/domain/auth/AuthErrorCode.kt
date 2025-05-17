7253197eebce4ebd82cbcd6fa62273f9a9fcfc7f
LEE YOU BIN <youbeene3409@naver.com>
2025-05-18 03:50:58 +0900

    feat(Auth) : resetRefreshToken 함수 구현

--- src/main/kotlin/com/terning/server/kotlin/domain/auth/AuthErrorCode.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/auth/AuthErrorCode.kt
@@ -7,6 +7,7 @@ enum class AuthErrorCode(
     val message: String,
 ) {
     INVALID_TOKEN(status = HttpStatus.UNAUTHORIZED, message = "유효하지 않은 토큰입니다."),
+    FAILED_REFRESH_TOKEN_RESET(status = HttpStatus.BAD_REQUEST, message = "리프레쉬 토큰 초기화에 실패하였습니다"),
     ;
 
     fun getErrorMessage(): String = message
