9702f497b566409992f509dbf59160ac5680b1fd
LEE YOU BIN <youbeene3409@naver.com>
2025-05-18 04:07:02 +0900

    feat(Auth) : ktlint 적용

--- src/main/kotlin/com/terning/server/kotlin/domain/auth/Auth.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/auth/Auth.kt
@@ -2,7 +2,15 @@ package com.terning.server.kotlin.domain.auth
 
 import com.terning.server.kotlin.domain.common.BaseRootEntity
 import com.terning.server.kotlin.domain.user.User
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "auth")
