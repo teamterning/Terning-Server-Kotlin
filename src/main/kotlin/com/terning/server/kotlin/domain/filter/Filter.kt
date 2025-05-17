f6ce61c14e49d726b6d7dfdb119688df97f8efd9
권장순 <jsoonworld@gmail.com>
2025-05-18 01:24:04 +0900

    feat(Filter) : Filter 도메인 구현 및 StartDate VO 적용

---
+++ src/main/kotlin/com/terning/server/kotlin/domain/filter/Filter.kt
@@ -0,0 +1,76 @@
+package com.terning.server.kotlin.domain.filter
+
+import com.terning.server.kotlin.domain.common.BaseRootEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Embedded
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "filters")
+class Filter private constructor(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private var jobType: JobType,
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private var grade: Grade,
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private var workingPeriod: WorkingPeriod,
+    @Embedded
+    private var startDate: StartDate,
+) : BaseRootEntity() {
+    companion object {
+        fun of(
+            jobType: JobType,
+            grade: Grade,
+            workingPeriod: WorkingPeriod,
+            startDate: StartDate,
+        ): Filter =
+            Filter(
+                jobType = jobType,
+                grade = grade,
+                workingPeriod = workingPeriod,
+                startDate = startDate,
+            )
+    }
+
+    fun update(
+        newJobType: JobType,
+        newGrade: Grade,
+        newWorkingPeriod: WorkingPeriod,
+        newStartDate: StartDate,
+    ) {
+        this.jobType = newJobType
+        this.grade = newGrade
+        this.workingPeriod = newWorkingPeriod
+        this.startDate = newStartDate
+    }
+
+    fun jobType(): JobType = jobType
+
+    fun grade(): Grade = grade
+
+    fun workingPeriod(): WorkingPeriod = workingPeriod
+
+    fun startDate(): StartDate = startDate
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as Filter
+        return id != null && id == other.id
+    }
+
+    override fun hashCode(): Int = id?.hashCode() ?: 0
+}
