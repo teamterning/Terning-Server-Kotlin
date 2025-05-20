c12b202aff7c64f73b7aa1387cc28c1a083b8fa2
권장순 <jsoonworld@gmail.com>
2025-05-20 23:18:51 +0900

    feat(InternshipAnnouncementYear) : 연도 VO 구현 및 유효성 검증 추가
    인턴십 시작 연도를 표현하는 값 객체 InternshipAnnouncementYear를 구현했습니다.
    생성 시 2025보다 큰 연도만 허용하도록 도메인 유효성 검증을 포함하였고,
    JPA 내장 타입으로 사용할 수 있도록 @Embeddable을 적용했습니다.

---
+++ src/main/kotlin/com/terning/server/kotlin/domain/internshipAnnouncement/InternshipAnnouncementYear.kt
@@ -0,0 +1,30 @@
+package com.terning.server.kotlin.domain.internshipAnnouncement
+
+import jakarta.persistence.Embeddable
+
+@Embeddable
+class InternshipAnnouncementYear private constructor(
+    val value: Int,
+) {
+    init {
+        validateYear(value)
+    }
+
+    companion object {
+        private const val MIN_VALID_YEAR = 2025
+
+        fun from(value: Int): InternshipAnnouncementYear = InternshipAnnouncementYear(value)
+
+        private fun validateYear(value: Int) {
+            if (value <= MIN_VALID_YEAR) {
+                throw InternshipException(InternshipErrorCode.INVALID_YEAR)
+            }
+        }
+    }
+
+    override fun equals(other: Any?): Boolean = this === other || (other is InternshipAnnouncementYear && value == other.value)
+
+    override fun hashCode(): Int = value
+
+    override fun toString(): String = value.toString()
+}
