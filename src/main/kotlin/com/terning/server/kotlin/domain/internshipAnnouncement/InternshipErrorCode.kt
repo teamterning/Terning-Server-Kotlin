dcc48f672dde44d4c9c04ab5afe1ac3b9ae7f3f3
권장순 <jsoonworld@gmail.com>
2025-05-21 00:53:30 +0900

    refactor(InternshipErrorCode) : 연도 유효성 메시지 기준 연도 2024로 수정

--- src/main/kotlin/com/terning/server/kotlin/domain/internshipAnnouncement/InternshipErrorCode.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/internshipAnnouncement/InternshipErrorCode.kt
@@ -18,7 +18,7 @@ enum class InternshipErrorCode(
     INVALID_INTERNSHIP_TITLE_TOO_LONG(HttpStatus.BAD_REQUEST, "인턴십 제목은 64자 이하여야 합니다."),
     INVALID_WORKING_PERIOD(HttpStatus.BAD_REQUEST, "근무 기간은 1개월 이상이어야 합니다."),
     INVALID_MONTH(HttpStatus.BAD_REQUEST, "월은 1~12 사이여야 합니다."),
-    INVALID_YEAR(HttpStatus.BAD_REQUEST, "연도는 2025보다 커야 합니다."),
+    INVALID_YEAR(HttpStatus.BAD_REQUEST, "연도는 2024보다 커야 합니다."),
     INVALID_COMPANY_LOGO_URL(HttpStatus.BAD_REQUEST, "올바른 이미지 URL이 아닙니다."),
     INVALID_ANNOUNCEMENT_URL(HttpStatus.BAD_REQUEST, "유효하지 않은 공고 URL입니다."),
 }
