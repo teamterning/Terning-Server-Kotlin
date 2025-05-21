731023f370824fd0885ea07fc215bf78896c6bc2
LEE YOU BIN <youbeene3409@naver.com>
2025-05-21 21:47:52 +0900

    feat(Auth) : ktlint 규칙에 맞게 수정

--- src/main/kotlin/com/terning/server/kotlin/domain/auth/Auth.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/auth/Auth.kt
@@ -7,13 +7,13 @@ import jakarta.persistence.Embedded
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.Table
-import jakarta.persistence.OneToOne
-import jakarta.persistence.FetchType
 import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "auth")
