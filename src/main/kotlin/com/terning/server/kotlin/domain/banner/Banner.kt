cd881920749e1ffa1db4064505f88fb69c3fb6d5
LEE YOU BIN <youbeene3409@naver.com>
2025-05-21 22:57:42 +0900

    feat(Banner) : ktlint 적용

--- src/main/kotlin/com/terning/server/kotlin/domain/banner/Banner.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/banner/Banner.kt
@@ -1,7 +1,14 @@
 package com.terning.server.kotlin.domain.banner
 
 import com.terning.server.kotlin.domain.common.BaseRootEntity
-import jakarta.persistence.*
+import jakarta.persistence.AttributeOverride
+import jakarta.persistence.Column
+import jakarta.persistence.Embedded
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "banners")
