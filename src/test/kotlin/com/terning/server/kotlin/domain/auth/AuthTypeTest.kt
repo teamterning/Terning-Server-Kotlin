40654b6bcaf3738749e5398ab2a034255694a1f4
LEE YOU BIN <youbeene3409@naver.com>
2025-05-21 20:32:24 +0900

    chore(AuthTypeTest) : 테스트 코드 흐름 수정

--- src/test/kotlin/com/terning/server/kotlin/domain/auth/AuthTypeTest.kt
+++ src/test/kotlin/com/terning/server/kotlin/domain/auth/AuthTypeTest.kt
@@ -3,17 +3,20 @@ package com.terning.server.kotlin.domain.auth
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.http.HttpStatus
 
 class AuthTypeTest {
     @Test
-    @DisplayName("prefix와 message 결합하여 반환한다")
-    fun getPrefixMessage() {
-        val errorCode = AuthErrorCode.INVALID_TOKEN
-        val expectedMessage = "[AUTH ERROR] 유효하지 않은 토큰입니다."
+    @DisplayName("유효하지 않은 토큰은 예외가 발생한다.")
+    fun throwAuthExceptionWhenTokenIsInvalid() {
+        // when
+        val exception = assertThrows<AuthException> {
+                throw AuthException(AuthErrorCode.INVALID_TOKEN)
+            }
 
-        assertThat(errorCode.getPrefixMessage()).isEqualTo(expectedMessage)
-        assertThat(errorCode.status).isEqualTo(HttpStatus.UNAUTHORIZED)
-        assertThat(errorCode.message).isEqualTo("유효하지 않은 토큰입니다.")
+        // then
+        assertThat(exception.message).isEqualTo("유효하지 않은 토큰입니다.")
+        assertThat(exception.errorCode.status).isEqualTo(HttpStatus.UNAUTHORIZED)
     }
 }
