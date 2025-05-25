656517a043b95d4eec59a5958767df5591a5cb06
LEE YOU BIN <youbeene3409@naver.com>
2025-05-25 14:20:45 +0900

    chore(ImageUrl) : Embeddable 어노테이션 추가

--- src/main/kotlin/com/terning/server/kotlin/domain/banner/ImageUrl.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/banner/ImageUrl.kt
@@ -1,7 +1,9 @@
 package com.terning.server.kotlin.domain.banner
 
 import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
 
+@Embeddable
 data class ImageUrl(
     @Column(length = 255)
     val value: String,
