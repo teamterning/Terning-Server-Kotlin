a01ca1e05c52251cb98f4e28e339ee3de186dad4
LEE YOU BIN <youbeene3409@naver.com>
2025-05-25 14:20:52 +0900

    chore(Link) : Embeddable 어노테이션 추가

--- src/main/kotlin/com/terning/server/kotlin/domain/banner/Link.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/banner/Link.kt
@@ -1,7 +1,9 @@
 package com.terning.server.kotlin.domain.banner
 
 import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
 
+@Embeddable
 data class Link(
     @Column(length = 255)
     val value: String,
