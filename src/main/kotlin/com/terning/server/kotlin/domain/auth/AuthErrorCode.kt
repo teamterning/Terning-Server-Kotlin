d9466b11176aaab0a2431d300bb7256125943964
LEE YOU BIN <youbeene3409@naver.com>
2025-05-25 13:35:28 +0900

    chore(AuthErrorCode) : BaseErrorCode 상속

--- src/main/kotlin/com/terning/server/kotlin/domain/auth/AuthErrorCode.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/auth/AuthErrorCode.kt
@@ -1,11 +1,12 @@
 package com.terning.server.kotlin.domain.auth
 
+import com.terning.server.kotlin.domain.common.BaseErrorCode
 import org.springframework.http.HttpStatus
 
 enum class AuthErrorCode(
-    val status: HttpStatus,
-    val message: String,
-) {
+    override val status: HttpStatus,
+    override val message: String,
+) : BaseErrorCode {
     INVALID_TOKEN(status = HttpStatus.UNAUTHORIZED, message = "유효하지 않은 토큰입니다."),
     ;
 
