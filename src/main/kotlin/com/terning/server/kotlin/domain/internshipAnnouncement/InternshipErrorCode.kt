0f7ed7fb2373c90a1645e2a6d8c647f55d87648d
권장순 <jsoonworld@gmail.com>
2025-05-27 10:06:37 +0900

    hitfix(Date) : 충돌 및 ktlint foramt 적용

--- src/main/kotlin/com/terning/server/kotlin/domain/internshipAnnouncement/InternshipErrorCode.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/internshipAnnouncement/InternshipErrorCode.kt
@@ -7,7 +7,7 @@ enum class InternshipErrorCode(
     override val status: HttpStatus,
     override val message: String,
 ) : BaseErrorCode {
-    INVALID_DEADLINE(HttpStatus.BAD_REQUEST, "마감일은 2025년 이후여야 합니다."),
+    INVALID_DEADLINE(HttpStatus.BAD_REQUEST, "마감일은 2024년 이후여야 합니다."),
     INVALID_SCRAP_COUNT(HttpStatus.BAD_REQUEST, "스크랩 수는 음수일 수 없습니다."),
     SCRAP_COUNT_CANNOT_BE_DECREASED_BELOW_ZERO(HttpStatus.BAD_REQUEST, "스크랩 수는 0보다 작아질 수 없습니다."),
     INVALID_VIEW_COUNT(HttpStatus.BAD_REQUEST, "조회수는 음수일 수 없습니다."),
@@ -18,5 +18,5 @@ enum class InternshipErrorCode(
     INVALID_INTERNSHIP_TITLE_TOO_LONG(HttpStatus.BAD_REQUEST, "인턴십 제목은 64자 이하여야 합니다."),
     INVALID_WORKING_PERIOD(HttpStatus.BAD_REQUEST, "근무 기간은 1개월 이상이어야 합니다."),
     INVALID_MONTH(HttpStatus.BAD_REQUEST, "월은 1~12 사이여야 합니다."),
-    INVALID_YEAR(HttpStatus.BAD_REQUEST, "연도는 2025보다 커야 합니다."),
+    INVALID_YEAR(HttpStatus.BAD_REQUEST, "연도는 2024보다 커야 합니다."),
 }
