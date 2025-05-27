28f37bed447367c839d3577e486f6c22776a919e
LEE YOU BIN <youbeene3409@naver.com>
2025-05-28 05:28:56 +0900

    chore(Banner) : 빈 줄 추가

--- src/main/kotlin/com/terning/server/kotlin/domain/banner/Banner.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/banner/Banner.kt
@@ -16,12 +16,15 @@ class Banner(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
+
     @Embedded
     @AttributeOverride(name = "value", column = Column(name = "imageUrl"))
     var imageUrl: ImageUrl,
+
     @Embedded
     @AttributeOverride(name = "value", column = Column(name = "link"))
     var link: Link,
+
     @Column
     private var priority: Int,
 ) : BaseRootEntity()
