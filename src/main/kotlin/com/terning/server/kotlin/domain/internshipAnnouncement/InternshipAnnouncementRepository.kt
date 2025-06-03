4ed3ba8aa97b53d78438fcd51805ce0fd165ed4a
권장순 <jsoonworld@gmail.com>
2025-06-03 14:59:05 +0900

    feat(InternshipAnnouncementRepository) : 인턴십 공고 리포지토리 인터페이스 생성
    - InternshipAnnouncement 엔티티 관리를 위한 JPA 리포지토리 정의

---
+++ src/main/kotlin/com/terning/server/kotlin/domain/internshipAnnouncement/InternshipAnnouncementRepository.kt
@@ -0,0 +1,5 @@
+package com.terning.server.kotlin.domain.internshipAnnouncement
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface InternshipAnnouncementRepository : JpaRepository<InternshipAnnouncement, Long>
