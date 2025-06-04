6eb4d02dc74f0e6952ed2e2df6d771d40c49a66b
LEE YOU BIN <youbeene3409@naver.com>
2025-06-04 14:50:26 +0900

    feat(ProfileServiceTest) : ProfileServiceTest 구현

---
+++ src/test/kotlin/com/terning/server/kotlin/application/profile/ProfileServiceTest.kt
@@ -0,0 +1,69 @@
+package com.terning.server.kotlin.application.profile
+
+import com.terning.server.kotlin.domain.auth.Auth
+import com.terning.server.kotlin.domain.auth.AuthRepository
+import com.terning.server.kotlin.domain.auth.exception.AuthErrorCode
+import com.terning.server.kotlin.domain.auth.exception.AuthException
+import com.terning.server.kotlin.domain.auth.vo.AuthId
+import com.terning.server.kotlin.domain.auth.vo.AuthType
+import com.terning.server.kotlin.domain.auth.vo.RefreshToken
+import com.terning.server.kotlin.domain.user.User
+import com.terning.server.kotlin.domain.user.vo.ProfileImage
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.mock
+import java.util.*
+
+class ProfileServiceTest {
+
+    private val authRepository : AuthRepository = mock()
+
+    private lateinit var profileService: ProfileService
+
+    @BeforeEach
+    fun setUp() {
+        profileService = ProfileService(authRepository)
+    }
+
+    @Test
+    @DisplayName("userId로 프로필 정보를 성공적으로 조회한다")
+    fun getProfileInformation() {
+        // given
+        val user = User.of(name = "유빈", profile = "BASIC")
+        val auth = Auth.of(
+            user = user,
+            authId = AuthId("123"),
+            authType = AuthType.KAKAO,
+            refreshToken = RefreshToken("refreshToken")
+        )
+        val userId = 1L
+        given(authRepository.findByUserId(userId)).willReturn(Optional.of(auth))
+
+        // when
+        val result = profileService.getProfile(userId)
+
+        // then
+        assertThat(result.name).isEqualTo("유빈")
+        assertThat(result.profileImage).isEqualTo(ProfileImage.BASIC)
+        assertThat(result.authType).isEqualTo(AuthType.KAKAO)
+    }
+
+    @Test
+    @DisplayName("userId로 조회 실패 시 예외를 던진다")
+    fun getProfileFailsIfUserNotFound() {
+        // given
+        val userId = 1L
+        given(authRepository.findByUserId(userId)).willReturn(Optional.empty())
+
+        // then
+        val exception = assertThrows(AuthException::class.java) {
+            profileService.getProfile(userId)
+        }
+
+        assertThat(exception.errorCode).isEqualTo(AuthErrorCode.NOT_FOUND_USER_EXCEPTION)
+    }
+}
