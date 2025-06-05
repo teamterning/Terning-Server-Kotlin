e7c7e4365d9cf90461c0d340058f390d56f75c67
LEE YOU BIN <youbeene3409@naver.com>
2025-06-06 04:48:25 +0900

    chore(AuthRepository) : user 함수 삭제

--- src/main/kotlin/com/terning/server/kotlin/domain/auth/AuthRepository.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/auth/AuthRepository.kt
@@ -1,8 +1,5 @@
 package com.terning.server.kotlin.domain.auth
 
-import com.terning.server.kotlin.domain.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface AuthRepository : JpaRepository<Auth, Long> {
-    fun user(user: User): MutableList<Auth>
-}
+interface AuthRepository : JpaRepository<Auth, Long>
