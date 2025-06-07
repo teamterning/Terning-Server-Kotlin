53caa015cdedfcdf904c5d7aa888fd99ee91f0b2
LEE YOU BIN <youbeene3409@naver.com>
2025-06-07 18:57:10 +0900

    feat(FilterService) : FilterService 구현

--- src/main/kotlin/com/terning/server/kotlin/application/filter/FilterService.kt
+++ src/main/kotlin/com/terning/server/kotlin/application/filter/FilterService.kt
@@ -1,8 +1,8 @@
 package com.terning.server.kotlin.application.filter
 
 import com.terning.server.kotlin.domain.filter.FilterRepository
-import com.terning.server.kotlin.domain.filter.exception.FilterErrorCode
-import com.terning.server.kotlin.domain.filter.exception.FilterException
+import com.terning.server.kotlin.domain.user.exception.UserErrorCode
+import com.terning.server.kotlin.domain.user.exception.UserException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -15,17 +15,15 @@ class FilterService(
     fun getUserFilter(userId: Long): FilterResponse {
         val filter =
             filterRepository.findById(userId).orElseThrow {
-                FilterException(FilterErrorCode.NOT_FOUND_USER_EXCEPTION)
+                UserException(UserErrorCode.NOT_FOUND_USER_EXCEPTION)
             }
-
-        val startDate = filter.startDate()
 
         return FilterResponse(
             jobType = filter.jobType().type,
             grade = filter.grade().type,
             workingPeriod = filter.workingPeriod().period,
-            startYear = startDate.filterYear.value,
-            startMonth = startDate.filterMonth.value,
+            startYear = filter.startDate().filterYear.value,
+            startMonth = filter.startDate().filterMonth.value,
         )
     }
 }
