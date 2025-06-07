87b90488ea0115c15ed59d44f42f31fa891b795f
권장순 <jsoonworld@gmail.com>
2025-06-07 23:44:43 +0900

    refactor(Company) : @Embedded 필드의 불필요한 @Column 애너테이션 제거

--- src/main/kotlin/com/terning/server/kotlin/domain/internshipAnnouncement/vo/Company.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/internshipAnnouncement/vo/Company.kt
@@ -1,6 +1,5 @@
 package com.terning.server.kotlin.domain.internshipAnnouncement.vo
 
-import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
 import jakarta.persistence.Embedded
 import jakarta.persistence.EnumType
@@ -9,15 +8,12 @@ import jakarta.persistence.Enumerated
 @Embeddable
 class Company private constructor(
     @Embedded
-    @Column(name = "companyInfo")
     val name: CompanyName,
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "companyCategory")
     val category: CompanyCategory,
 
     @Embedded
-    @Column(name = "companyImage")
     val logoUrl: CompanyLogoUrl,
 ) {
     override fun equals(other: Any?): Boolean =
