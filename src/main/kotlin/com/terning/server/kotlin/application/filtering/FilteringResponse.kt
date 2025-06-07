0feb32689e44b900c748d1b9a0c4197faf90fe38
LEE YOU BIN <youbeene3409@naver.com>
2025-06-07 18:38:02 +0900

    feat(FilterResponse) : Filter 네이밍 변경
    # Conflicts:
    #	src/main/kotlin/com/terning/server/kotlin/application/filter/FilterResponse.kt

--- src/main/kotlin/com/terning/server/kotlin/application/filtering/FilteringResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.terning.server.kotlin.application.filtering
-
-data class FilteringResponse (
-    val jobType: String,
-    val grade: String,
-    val workingPeriod: String,
-    val startYear: Int,
-    val startMonth: Int,
-)
