0e225e7b131fdaf2f3d94d2b1ac2c59ca72921b4
LEE YOU BIN <youbeene3409@naver.com>
2025-06-07 17:19:36 +0900

    feat(ProfileController) : controller 작성

--- src/main/kotlin/com/terning/server/kotlin/ui/api/ProfileController.kt
+++ src/main/kotlin/com/terning/server/kotlin/ui/api/ProfileController.kt
@@ -1,10 +1,13 @@
 package com.terning.server.kotlin.ui.api
 
+import com.terning.server.kotlin.application.profile.ProfileRequest
 import com.terning.server.kotlin.application.profile.ProfileResponse
 import com.terning.server.kotlin.application.profile.ProfileService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -30,5 +33,23 @@ class ProfileController(
                     result = response,
                 ),
             )
+    }
+
+    @PatchMapping("mypage/profile")
+    fun updateProfile(
+        // TODO : @AuthenticationPrincipal userId: Long,
+        @RequestBody profileRequest: ProfileRequest,
+    ): ResponseEntity<ApiResponse<Unit>> {
+        val userId: Long = 1 // TODO: @AuthenticationPrincipal 구현 시 제거
+
+        profileService.updateUserProfile()
+
+        return ResponseEntity.ok(
+            ApiResponse.success(
+                status = HttpStatus.OK,
+                message = "프로필 수정에 성공했습니다",
+                result = Unit,
+            ),
+        )
     }
 }
