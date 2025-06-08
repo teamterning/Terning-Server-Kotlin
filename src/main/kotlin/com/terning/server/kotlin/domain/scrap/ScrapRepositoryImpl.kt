19a96c87305475ce38ab12b387cfd6ab35ff1b34
권장순 <jsoonworld@gmail.com>
2025-06-08 17:43:46 +0900

    feat(ScrapRepositoryImpl) : 일간 스크랩 조회 쿼리 구현

--- src/main/kotlin/com/terning/server/kotlin/domain/scrap/ScrapRepositoryImpl.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/scrap/ScrapRepositoryImpl.kt
@@ -24,4 +24,20 @@ class ScrapRepositoryImpl(
             .orderBy(scrap.internshipAnnouncement.internshipAnnouncementDeadline.value.asc())
             .fetch()
     }
+
+    override fun findScrapsByUserIdAndDeadlineOrderByDeadline(
+        userId: Long,
+        date: LocalDate,
+    ): List<Scrap> {
+        val scrap = QScrap.scrap
+
+        return queryFactory
+            .selectFrom(scrap)
+            .where(
+                scrap.user.id.eq(userId)
+                    .and(scrap.internshipAnnouncement.internshipAnnouncementDeadline.value.eq(date)),
+            )
+            .orderBy(scrap.internshipAnnouncement.internshipAnnouncementDeadline.value.asc())
+            .fetch()
+    }
 }
