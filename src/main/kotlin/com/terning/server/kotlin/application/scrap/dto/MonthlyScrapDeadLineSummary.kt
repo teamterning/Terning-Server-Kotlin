117b5a3a3aabe605fb569a696bec58eaa1dbf0fb
권장순 <jsoonworld@gmail.com>
2025-06-08 15:48:53 +0900

    refactor(MonthlyScrapDeadlineResponse) : 중첩 DTO를 동일 파일 내 평면 구조로 분리

--- src/main/kotlin/com/terning/server/kotlin/application/scrap/dto/MonthlyScrapDeadLineSummary.kt
+++
@@ -1,7 +0,0 @@
-package com.terning.server.kotlin.application.scrap.dto
-
-data class MonthlyScrapDeadLineSummary(
-    val scrapId: Long,
-    val title: String,
-    val color: String,
-)
--- src/main/kotlin/com/terning/server/kotlin/application/scrap/dto/MonthlyScrapDeadlineGroup.kt
+++
@@ -1,6 +0,0 @@
-package com.terning.server.kotlin.application.scrap.dto
-
-data class MonthlyScrapDeadlineGroup(
-    val deadline: String,
-    val scraps: List<MonthlyScrapDeadLineSummary>,
-)
