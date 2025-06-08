1d8f2ad6a30424cb0bf38a0df62f87f3a96509d6
LEE YOU BIN <youbeene3409@naver.com>
2025-06-08 13:22:09 +0900

    feat(FilterServiceTest) : service 테스트코드 작성

---
+++ src/test/kotlin/com/terning/server/kotlin/application/filter/FilterServiceTest.kt
@@ -0,0 +1,72 @@
+package com.terning.server.kotlin.application.filter
+
+import com.terning.server.kotlin.domain.filter.Filter
+import com.terning.server.kotlin.domain.filter.FilterRepository
+import com.terning.server.kotlin.domain.filter.vo.*
+import com.terning.server.kotlin.domain.user.exception.UserErrorCode
+import com.terning.server.kotlin.domain.user.exception.UserException
+import io.mockk.every
+import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.util.Optional
+
+class FilterServiceTest {
+    private val filterRepository: FilterRepository = mockk()
+
+    private lateinit var filterService: FilterService
+
+    @BeforeEach
+    fun setUp() {
+        filterService = FilterService(filterRepository = filterRepository)
+    }
+
+    @Test
+    @DisplayName("사용자를 찾을 수 없으면 에러가 발생한다")
+    fun getFilterFailsIfUserNotFound() {
+        // given
+        val userId = 1L
+        every { filterRepository.findById(userId) } returns Optional.empty()
+
+        // then
+        val exception =
+            assertThrows(UserException::class.java) {
+                filterService.getUserFilter(userId)
+            }
+
+        assertThat(exception.errorCode).isEqualTo(UserErrorCode.NOT_FOUND_USER_EXCEPTION)
+    }
+
+    @Test
+    @DisplayName("필터 정보를 성공적으로 조회한다")
+    fun getFilterInformation() {
+        // given
+        val userId = 1L
+        val filter =
+            Filter.of(
+                filterJobType = FilterJobType.IT,
+                filterGrade = FilterGrade.SENIOR,
+                filterWorkingPeriod = FilterWorkingPeriod.SHORT_TERM,
+                filterStartDate =
+                    FilterStartDate.of(
+                        filterYear = FilterYear.from(2025),
+                        filterMonth = FilterMonth.from(6),
+                    ),
+            )
+
+        every { filterRepository.findById(userId) } returns Optional.of(filter)
+
+        // when
+        val result = filterService.getUserFilter(userId)
+
+        // then
+        assertThat(result.jobType).isEqualTo("it")
+        assertThat(result.grade).isEqualTo("senior")
+        assertThat(result.workingPeriod).isEqualTo("short")
+        assertThat(result.startYear).isEqualTo(2025)
+        assertThat(result.startMonth).isEqualTo(6)
+    }
+}
