0d3db37f180493d011c85c9f6a2ad33f53e068ca
권장순 <jsoonworld@gmail.com>
2025-06-10 11:32:47 +0900

    feat(Filter) : 유저 연관관계 및 기본 필터 여부 판단 로직 추가

--- src/main/kotlin/com/terning/server/kotlin/domain/filter/Filter.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/filter/Filter.kt
@@ -3,8 +3,11 @@ package com.terning.server.kotlin.domain.filter
 import com.terning.server.kotlin.domain.common.BaseRootEntity
 import com.terning.server.kotlin.domain.filter.vo.FilterGrade
 import com.terning.server.kotlin.domain.filter.vo.FilterJobType
+import com.terning.server.kotlin.domain.filter.vo.FilterMonth
 import com.terning.server.kotlin.domain.filter.vo.FilterStartDate
 import com.terning.server.kotlin.domain.filter.vo.FilterWorkingPeriod
+import com.terning.server.kotlin.domain.filter.vo.FilterYear
+import com.terning.server.kotlin.domain.user.User
 import jakarta.persistence.AttributeOverride
 import jakarta.persistence.AttributeOverrides
 import jakarta.persistence.Column
@@ -12,9 +15,12 @@ import jakarta.persistence.Embedded
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 
 @Entity
@@ -23,6 +29,10 @@ class Filter private constructor(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
 
     @Enumerated(EnumType.STRING)
     private var filterJobType: FilterJobType,
@@ -60,10 +70,16 @@ class Filter private constructor(
 
     fun startDate(): FilterStartDate = filterStartDate
 
+    fun isDefault(): Boolean =
+        jobType() == FilterJobType.TOTAL &&
+            grade() == FilterGrade.NONE &&
+            workingPeriod() == FilterWorkingPeriod.NONE &&
+            startDate().filterYear == FilterYear.DEFAULT &&
+            startDate().filterMonth == FilterMonth.DEFAULT
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || this::class != other::class) return false
-
         other as Filter
         return id != null && id == other.id
     }
@@ -72,12 +88,14 @@ class Filter private constructor(
 
     companion object {
         fun of(
+            user: User,
             filterJobType: FilterJobType,
             filterGrade: FilterGrade,
             filterWorkingPeriod: FilterWorkingPeriod,
             filterStartDate: FilterStartDate,
         ): Filter =
             Filter(
+                user = user,
                 filterJobType = filterJobType,
                 filterGrade = filterGrade,
                 filterWorkingPeriod = filterWorkingPeriod,
