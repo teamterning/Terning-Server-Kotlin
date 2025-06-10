8e00e8d841989ee26d5da11cc39da920c33bbd24
권장순 <jsoonworld@gmail.com>
2025-06-10 11:51:18 +0900

    feat(InternshipAnnouncementRepository) : 커스텀 레포지토리 인터페이스 확장

--- src/main/kotlin/com/terning/server/kotlin/domain/internshipAnnouncement/InternshipAnnouncementRepository.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/internshipAnnouncement/InternshipAnnouncementRepository.kt
@@ -2,4 +2,4 @@ package com.terning.server.kotlin.domain.internshipAnnouncement
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface InternshipAnnouncementRepository : JpaRepository<InternshipAnnouncement, Long>
+interface InternshipAnnouncementRepository : JpaRepository<InternshipAnnouncement, Long>, InternshipAnnouncementRepositoryCustom
