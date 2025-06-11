c0990b07ae31ceaf0432b0bbaba7511f4d0b7650
LEE YOU BIN <youbeene3409@naver.com>
2025-06-12 02:17:05 +0900

    feat(FilterController) : FilterController post 구현

--- src/main/kotlin/com/terning/server/kotlin/ui/api/FilterController.kt
+++ src/main/kotlin/com/terning/server/kotlin/ui/api/FilterController.kt
@@ -5,6 +5,7 @@ import com.terning.server.kotlin.application.filter.FilterService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -25,6 +26,23 @@ class FilterController(
             ApiResponse.success(
                 status = HttpStatus.OK,
                 message = "사용자의 필터링 정보를 불러오는데 성공했습니다",
+                result = response,
+            ),
+        )
+    }
+
+    @PutMapping("filters")
+    fun putUserFilter(
+        // TODO: @AuthenticationPrincipal userId: Long,
+    ): ResponseEntity<ApiResponse<FilterResponse>> {
+        val userId: Long = 1 // TODO: @AuthenticationPrincipal 구현 시 제거
+
+        val response = filterService.postUserFilter(userId)
+
+        return ResponseEntity.ok(
+            ApiResponse.success(
+                status = HttpStatus.OK,
+                message = "필터링 재설정에 성공했습니다",
                 result = response,
             ),
         )
