e817242b2d9353ea2464fdf2cdd1efcce9eda242
권장순 <jsoonworld@gmail.com>
2025-06-11 12:03:54 +0900

    refactor(filterService) : 테스트 수정

--- src/test/kotlin/com/terning/server/kotlin/application/filter/FilterServiceTest.kt
+++ src/test/kotlin/com/terning/server/kotlin/application/filter/FilterServiceTest.kt
@@ -11,6 +11,7 @@ import com.terning.server.kotlin.domain.filter.vo.FilterStartDate
 import com.terning.server.kotlin.domain.filter.vo.FilterWorkingPeriod
 import com.terning.server.kotlin.domain.filter.vo.FilterYear
 import com.terning.server.kotlin.domain.user.User
+import com.terning.server.kotlin.domain.user.UserRepository
 import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
@@ -22,12 +23,13 @@ import java.util.Optional
 
 class FilterServiceTest {
     private val filterRepository: FilterRepository = mockk()
+    private val userRepository: UserRepository = mockk()
 
     private lateinit var filterService: FilterService
 
     @BeforeEach
     fun setUp() {
-        filterService = FilterService(filterRepository = filterRepository)
+        filterService = FilterService(filterRepository, userRepository)
     }
 
     @Test
@@ -35,7 +37,7 @@ class FilterServiceTest {
     fun getFilterFailsIfUserNotFound() {
         // given
         val userId = 1L
-        every { filterRepository.findById(userId) } returns Optional.empty()
+        every { userRepository.findById(userId) } returns Optional.empty()
 
         // when & then
         val exception =
@@ -44,6 +46,25 @@ class FilterServiceTest {
             }
 
         assertThat(exception.errorCode).isEqualTo(FilterErrorCode.NOT_FOUND_USER_EXCEPTION)
+    }
+
+    @Test
+    @DisplayName("사용자는 있지만 필터가 없으면 에러가 발생한다")
+    fun getFilterFailsIfFilterNotFound() {
+        // given
+        val userId = 1L
+        val user = mockk<User>()
+
+        every { userRepository.findById(userId) } returns Optional.of(user)
+        every { filterRepository.findLatestByUser(user) } returns null
+
+        // when & then
+        val exception =
+            assertThrows(FilterException::class.java) {
+                filterService.getUserFilter(userId)
+            }
+
+        assertThat(exception.errorCode).isEqualTo(FilterErrorCode.NOT_FOUND_FILTER_EXCEPTION)
     }
 
     @Test
@@ -65,7 +86,8 @@ class FilterServiceTest {
                     ),
             )
 
-        every { filterRepository.findById(userId) } returns Optional.of(filter)
+        every { userRepository.findById(userId) } returns Optional.of(user)
+        every { filterRepository.findLatestByUser(user) } returns filter
 
         // when
         val result = filterService.getUserFilter(userId)
