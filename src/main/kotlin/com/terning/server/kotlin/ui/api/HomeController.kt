f43f2e40674d4704d4af8045ba0324d47dcb57c0
LEE YOU BIN <youbeene3409@naver.com>
2025-06-17 16:25:43 +0900

    feat(All) : 컨플릭 해결

--- src/main/kotlin/com/terning/server/kotlin/ui/api/HomeController.kt
+++ src/main/kotlin/com/terning/server/kotlin/ui/api/HomeController.kt
@@ -8,7 +8,6 @@ import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -18,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController
 class HomeController(
     private val homeService: HomeService,
 ) {
-    @GetMapping("home")
+    @GetMapping
     fun getInternshipAnnouncementsFilteredByUserFilter(
         // TODO: 실제 로그인된 사용자의 인증 정보 주입 필요
         // @AuthenticationPrincipal userId: Long,
