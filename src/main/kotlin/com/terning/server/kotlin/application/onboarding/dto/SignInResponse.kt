82c862e5318a9735e0f87c0615bdd9e7d331ed4a
LEE YOU BIN <youbeene3409@naver.com>
2025-06-18 04:49:28 +0900

    feat(SignInResponse) : SignInResponse 구현

---
+++ src/main/kotlin/com/terning/server/kotlin/application/onboarding/dto/SignInResponse.kt
@@ -0,0 +1,10 @@
+package com.terning.server.kotlin.application.onboarding.dto
+
+data class SignInResponse (
+    val accessToken: String,
+    val refreshToken: String,
+    val userId: Long,
+    val authId: String,
+    val authType: String,
+    val fcmTokenReissueRequired: Boolean,
+)
