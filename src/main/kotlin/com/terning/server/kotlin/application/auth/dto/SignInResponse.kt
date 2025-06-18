67d30567d3ca582f32284e6bef5ddbefa47e2004
LEE YOU BIN <youbeene3409@naver.com>
2025-06-19 06:56:57 +0900

    feat(SignInResponse) : 응답값 작성

---
+++ src/main/kotlin/com/terning/server/kotlin/application/auth/dto/SignInResponse.kt
@@ -0,0 +1,32 @@
+package com.terning.server.kotlin.application.auth.dto
+
+import com.terning.server.kotlin.domain.auth.vo.AuthType
+import com.terning.server.kotlin.domain.auth.vo.Token
+
+data class SignInResponse(
+    val accessToken: String?,
+    val refreshToken: String?,
+    val userId: Long?,
+    val authId: String,
+    val authType: String,
+    val fcmTokenReissueRequired: Boolean,
+) {
+    companion object {
+        fun of(
+            token: Token?,
+            authId: String,
+            userId: Long?,
+            authType: AuthType,
+            fcmTokenReissueRequired: Boolean = false,
+        ): SignInResponse {
+            return SignInResponse(
+                accessToken = token?.accessToken,
+                refreshToken = token?.refreshToken,
+                userId = userId,
+                authId = authId,
+                authType = authType.name,
+                fcmTokenReissueRequired = fcmTokenReissueRequired,
+            )
+        }
+    }
+}
