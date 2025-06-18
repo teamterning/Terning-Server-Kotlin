89824febf73c4b9ceda19ce9033bbb0c47f4c327
LEE YOU BIN <youbeene3409@naver.com>
2025-06-19 06:57:45 +0900

    feat(AuthRepository) : 함수 경우 추가

--- src/main/kotlin/com/terning/server/kotlin/domain/auth/AuthRepository.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/auth/AuthRepository.kt
@@ -1,5 +1,12 @@
 package com.terning.server.kotlin.domain.auth
 
+import com.terning.server.kotlin.domain.auth.vo.AuthId
+import com.terning.server.kotlin.domain.auth.vo.AuthType
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface AuthRepository : JpaRepository<Auth, Long>
+interface AuthRepository : JpaRepository<Auth, Long> {
+    fun findByAuthIdAndAuthType(
+        authId: AuthId,
+        authType: AuthType,
+    ): Auth?
+}
