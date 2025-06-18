d30c92d2553f67e56531180d9d3574b70f8d4e3c
LEE YOU BIN <youbeene3409@naver.com>
2025-06-19 06:52:36 +0900

    feat(AuthId) : data class -> class 변경

--- src/main/kotlin/com/terning/server/kotlin/domain/auth/vo/AuthId.kt
+++ src/main/kotlin/com/terning/server/kotlin/domain/auth/vo/AuthId.kt
@@ -3,6 +3,16 @@ package com.terning.server.kotlin.domain.auth.vo
 import jakarta.persistence.Embeddable
 
 @Embeddable
-data class AuthId(
+class AuthId private constructor(
     val value: String,
-)
+) {
+    override fun equals(other: Any?): Boolean = this === other || (other is AuthId && value == other.value)
+
+    override fun hashCode(): Int = value.hashCode()
+
+    override fun toString(): String = "AuthId(value=$value)"
+
+    companion object {
+        fun from(value: String): AuthId = AuthId(value)
+    }
+}
