9344bcaccdde7a4ebbd36d81b3b4718faa366465
LEE YOU BIN <youbeene3409@naver.com>
2025-06-19 07:50:24 +0900

    feat(AuthService) : AuthService 구현

--- src/main/kotlin/com/terning/server/kotlin/application/auth/AuthService.kt
+++ src/main/kotlin/com/terning/server/kotlin/application/auth/AuthService.kt
@@ -2,21 +2,60 @@ package com.terning.server.kotlin.application.auth
 
 import com.terning.server.kotlin.application.auth.dto.SignInRequest
 import com.terning.server.kotlin.application.auth.dto.SignInResponse
-import com.terning.server.kotlin.application.auth.signin.AuthSignInService
+import com.terning.server.kotlin.application.auth.social.SocialAuthServiceManager
+import com.terning.server.kotlin.domain.auth.AuthRepository
+import com.terning.server.kotlin.domain.auth.vo.AuthId
+import com.terning.server.kotlin.domain.auth.vo.AuthType
+import com.terning.server.kotlin.domain.auth.vo.RefreshToken
+import com.terning.server.kotlin.domain.common.security.jwt.application.JwtTokenManager
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional(readOnly = true)
 class AuthService(
-    private val authSignInService: AuthSignInService,
+    private val socialAuthServiceManager: SocialAuthServiceManager,
+    private val jwtTokenManager: JwtTokenManager,
+    private val authRepository: AuthRepository,
 ) {
     fun signInUser(
         authAccessToken: String,
         signInRequest: SignInRequest,
-    ): SignInResponse =
-        authSignInService.signIn(
-            authAccessToken = authAccessToken,
-            request = signInRequest,
+    ): SignInResponse {
+        val authType = AuthType.from(signInRequest.authType)
+        val authId =
+            socialAuthServiceManager.getAuthService(
+                authType = authType,
+            ).getAuthId(
+                authAccessToken = authAccessToken,
+            )
+        val auth =
+            authRepository.findByAuthIdAndAuthType(
+                authId = AuthId.from(authId),
+                authType = authType,
+            )
+
+        if (auth == null) {
+            return SignInResponse.of(
+                token = null,
+                userId = null,
+                authId = authId,
+                authType = authType,
+            )
+        }
+
+        val user = auth.user
+        val token = jwtTokenManager.generateToken(user)
+
+        auth.updateRefreshToken(
+            newRefreshToken = RefreshToken.from(token.refreshToken)
         )
+
+        return SignInResponse.of(
+            token = token,
+            authId = authId,
+            authType = authType,
+            userId = user.id,
+        )
+    }
 }
