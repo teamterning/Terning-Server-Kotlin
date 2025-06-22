7a880222b2a0f6e0086758641e6d691bed031853
LEE YOU BIN <youbeene3409@naver.com>
2025-06-23 04:01:57 +0900

    feat(SignUpResponse) : from 함수 구현

--- src/main/kotlin/com/terning/server/kotlin/application/auth/dto/SignUpResponse.kt
+++ src/main/kotlin/com/terning/server/kotlin/application/auth/dto/SignUpResponse.kt
@@ -1,8 +1,25 @@
 package com.terning.server.kotlin.application.auth.dto
 
-data class SignUpResponse (
+import com.terning.server.kotlin.domain.auth.vo.AuthType
+import com.terning.server.kotlin.domain.auth.vo.Token
+
+data class SignUpResponse(
     val accessToken: String,
     val refreshToken: String,
     val userId: Long,
     val authType: String,
-)
+) {
+    companion object {
+        fun from(
+            token: Token,
+            userId: Long,
+            authType: AuthType,
+        ): SignUpResponse =
+            SignUpResponse(
+                accessToken = token.accessToken,
+                refreshToken = token.refreshToken,
+                userId = userId,
+                authType = authType.name,
+            )
+    }
+}
