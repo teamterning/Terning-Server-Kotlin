3ef53c825b0835d20ef296c0c794d2c70632a0d4
권장순 <jsoonworld@gmail.com>
2025-06-28 19:48:46 +0900

    refactor(develop) : merge from develop

--- src/test/kotlin/com/terning/server/kotlin/ui/api/AuthControllerTest.kt
+++ src/test/kotlin/com/terning/server/kotlin/ui/api/AuthControllerTest.kt
@@ -22,6 +22,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
